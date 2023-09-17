dac0cc9476bcacba38bb5ed643dd266eac88c988
sazid526 <arefin15-14526@diu.edu.bd>
2023-09-17 23:57:02 +0600

    done

--- lib/main.dart
+++ lib/main.dart
@@ -18,6 +18,12 @@ class MyApp extends StatelessWidget {
 class Home extends StatelessWidget {
   const Home({Key? key}) : super(key: key);
 
+  MySnackBar(massage,context){
+    return ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text(massage))
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -25,39 +31,188 @@ class Home extends StatelessWidget {
         backgroundColor: Colors.blue,
         title: Center(
             child: Text(
-              "My Shopping List",
+              "Photo Gallery",
               style: TextStyle(fontSize: 25, fontWeight: FontWeight.normal),
             )),
-        actions: [
-          Padding(
-            padding: const EdgeInsets.all(10.0),
-            child: Icon(Icons.shopping_cart,size: 30,),
-          )
-        ],
-
       ),
-      body: ListView(
+      body:Column(
         children: [
-          ListTile(
-            leading: Icon(Icons.home),
-            title: Text("Home"),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Container(
+                    child: Text("Welcome To My Photo Gallery!",style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold
+                    ),)
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20,vertical: 7),
+                child: TextFormField(
+                  decoration: InputDecoration(
+                      hintText: "Search for photos...",
+                      enabledBorder: OutlineInputBorder(
+
+                      )
+                  ),
+                ),
+              ),
+            ],
           ),
-          ListTile(
-            leading: Icon(Icons.person),
-            title: Text("Person"),
+          Expanded(
+            flex: 18,
+            child: Container(
+              height: 40,
+              padding: EdgeInsets.all(10),
+              child: GridView(
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 3,
+                  ),
+                children: [
+                  Column(
+                    children: [
+                      InkWell(
+                        onTap: (){
+                          MySnackBar("Photo 0 Clicked", context);
+                        },
+                          child: Image.network("https://images.unsplash.com/photo-1554080353-a576cf803bda?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8cGhvdG98ZW58MHx8MHx8fDA%3D&w=1000&q=80",height: 100,width: 80,fit: BoxFit.fill,)),
+                      Padding(
+                        padding: const EdgeInsets.all(3.0),
+                        child: Center(child: Text("Photo 0",)),
+                      )
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      InkWell(
+                          onTap: (){
+                            MySnackBar("Photo 1 Clicked", context);
+                          },
+                          child: Image.network("https://images.unsplash.com/photo-1554080353-a576cf803bda?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8cGhvdG98ZW58MHx8MHx8fDA%3D&w=1000&q=80",height: 100,width: 80,fit: BoxFit.fill,)),
+                      Padding(
+                        padding: const EdgeInsets.all(3.0),
+                        child: Center(child: Text("Photo 1",)),
+                      )
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      InkWell(
+                          onTap: (){
+                            MySnackBar("Photo 2 Clicked", context);
+                          },
+                          child: Image.network("https://images.unsplash.com/photo-1554080353-a576cf803bda?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8cGhvdG98ZW58MHx8MHx8fDA%3D&w=1000&q=80",height: 100,width: 80,fit: BoxFit.fill,)),
+                      Padding(
+                        padding: const EdgeInsets.all(3.0),
+                        child: Center(child: Text("Photo 2",)),
+                      )
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      InkWell(
+                          onTap: (){
+                            MySnackBar("Photo 3 Clicked", context);
+                          },
+                          child: Image.network("https://images.unsplash.com/photo-1554080353-a576cf803bda?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8cGhvdG98ZW58MHx8MHx8fDA%3D&w=1000&q=80",height: 100,width: 80,fit: BoxFit.fill,)),
+                      Padding(
+                        padding: const EdgeInsets.all(3.0),
+                        child: Center(child: Text("Photo 3",)),
+                      )
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      InkWell(
+                          onTap: (){
+                            MySnackBar("Photo 4 Clicked", context);
+                          },
+                          child: Image.network("https://images.unsplash.com/photo-1554080353-a576cf803bda?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8cGhvdG98ZW58MHx8MHx8fDA%3D&w=1000&q=80",height: 100,width: 80,fit: BoxFit.fill,)),
+                      Padding(
+                        padding: const EdgeInsets.all(3.0),
+                        child: Center(child: Text("Photo 4",)),
+                      )
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      InkWell(
+                          onTap: (){
+                            MySnackBar("Photo 5 Clicked", context);
+                          },
+                          child: Image.network("https://images.unsplash.com/photo-1554080353-a576cf803bda?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8cGhvdG98ZW58MHx8MHx8fDA%3D&w=1000&q=80",height: 100,width: 80,fit: BoxFit.fill,)),
+                      Padding(
+                        padding: const EdgeInsets.all(3.0),
+                        child: Center(child: Text("Photo 5",)),
+                      )
+                    ],
+                  ),
+                ],
+              ),
+            ),
           ),
-          ListTile(
-            leading: Icon(Icons.call),
-            title: Text("Call"),
+          Expanded(
+            flex: 13,
+            child: ListView(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 15),
+                  child: ListTile(
+                    leading: CircleAvatar(
+                      radius: 20.0,
+                      backgroundImage: NetworkImage("https://images.unsplash.com/photo-1554080353-a576cf803bda?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8cGhvdG98ZW58MHx8MHx8fDA%3D&w=1000&q=80"),
+                    ),
+                    title: Text("Photo 1"),
+                    subtitle: Text("Photo 2"),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 15),
+                  child: ListTile(
+                    leading: CircleAvatar(
+                      radius: 20.0,
+                      backgroundImage: NetworkImage("https://images.unsplash.com/photo-1554080353-a576cf803bda?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8cGhvdG98ZW58MHx8MHx8fDA%3D&w=1000&q=80"),
+                    ),
+                    title: Text("Photo 1"),
+                    subtitle: Text("Photo 2"),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 15),
+                  child: ListTile(
+                    leading: CircleAvatar(
+                      radius: 20.0,
+                      backgroundImage: NetworkImage("https://images.unsplash.com/photo-1554080353-a576cf803bda?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8cGhvdG98ZW58MHx8MHx8fDA%3D&w=1000&q=80"),
+                    ),
+                    title: Text("Photo 1"),
+                    subtitle: Text("Photo 2"),
+                  ),
+                ),
+              ],
+            ),
           ),
-          ListTile(
-            leading: Icon(Icons.notification_important),
-            title: Text("Notification"),
-          ),
-          ListTile(
-            leading: Icon(Icons.business_center),
-            title: Text("Business"),
-          ),
+          SizedBox(height: 14,),
+          Container(
+            margin: EdgeInsets.symmetric(vertical: 5),
+            child: InkWell(
+              onTap: (){
+                MySnackBar("Photos Uploaded Successfully!", context);
+              },
+              child: Container(
+                width: 50,
+                height: 50,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Colors.blue
+                ),
+                child: Center(child: Icon(Icons.arrow_upward,color: Colors.white,)),
+
+              ),
+            )
+          )
         ],
       )
     );
