23ba5a05dc3fb3d2b8cc5622b9eff4d362516547
sazid526 <arefin15-14526@diu.edu.bd>
2023-09-25 00:02:42 +0600

    done

--- lib/home.dart
+++ lib/home.dart
@@ -1,6 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:ostad_assignments/cart.dart';
 
+class Product {
+  final String name;
+  final double price;
+
+  Product({required this.name, required this.price});
+}
+
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
 
@@ -9,6 +16,21 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  List<Product> products = [
+    Product(name: "product 1", price: 10.0),
+    Product(name: "Product 2", price: 20.0),
+    Product(name: "product 3", price: 60.0),
+    Product(name: "Product 4", price: 20.0),
+    Product(name: "product 5", price: 90.0),
+    Product(name: "Product 6", price: 50.0),
+    Product(name: "product 7", price: 16.0),
+    Product(name: "Product 8", price: 80.0),
+    Product(name: "product 9", price: 60.0),
+    Product(name: "Product 10", price: 20.0),
+  ];
+
+  Map<String, int> counters ={};
+
   MyAlartDialog(context) {
     showDialog(
         context: context,
@@ -25,90 +47,70 @@ class _HomeState extends State<Home> {
             ],
           );
         });
-
   }
 
-  int counter = 0;
-
-
-  var product = [
-    {"id": 1, "price": 25},
-    {"id": 2, "price": 22},
-    {"id": 3, "price": 15},
-    {"id": 4, "price": 28},
-    {"id": 5, "price": 13},
-    {"id": 6, "price": 20},
-    {"id": 7, "price": 31},
-    {"id": 8, "price": 19},
-    {"id": 9, "price": 22},
-    {"id": 10, "price": 90},
-  ];
-  
-
-  List<int> counters = [];
 
   @override
   Widget build(BuildContext context) {
-
-
-
     return Scaffold(
-        appBar: AppBar(
-          title: Center(child: Text("Product List")),
-        ),
-        body: ListView.builder(
-          itemCount: product.length,
-            itemBuilder: (context, index){
-              return ListTile(
-                title: Text("Product ${product[index]["id"]}"),
-                subtitle: Text("\$${product[index]["price"]}"),
-                trailing: Column(
-                  children: [
-                    Text("Counter : ${counter}"),
-                    InkWell(
-                      onTap: () {
-                        if(counter > 0){
-                          counters.add(counter);
-                        }
+      appBar: AppBar(
+        title: Center(child: Text("Product List")),
+      ),
+      body: ListView.builder(
+          itemCount: products.length,
+          itemBuilder: (context, index) {
+            final product = products[index];
+            final counter = counters[product.name] ?? 0;
 
-                        if (counter == 5) {
-                          MyAlartDialog(context);
-                        }
 
-                        setState(() {
-                          if (counter < 5) {
-                            counter = counter + 1;
-                          }
-                        });
-                      },
-                      child: Container(
-                        height: 30,
-                        width: 100,
-                        child: Center(
-                          child: Text(
-                            "Buy Now",
-                            style: TextStyle(color: Colors.white),
-                          ),
+            return ListTile(
+              title: Text("${product.name}"),
+              subtitle: Text("\$${product.price}"),
+              trailing: Column(
+                children: [
+                  Text("Counter : $counter"),
+                  InkWell(
+                    onTap: () {
+                      if (counter == 5) {
+                        MyAlartDialog(context);
+                      }
+                      setState(() {
+                        if (counter < 5) {
+                          counters[product.name] = counter + 1;
+                        }
+                      });
+                    },
+                    child: Container(
+                      height: 30,
+                      width: 100,
+                      child: Center(
+                        child: Text(
+                          "Buy Now",
+                          style: TextStyle(color: Colors.white),
                         ),
-                        padding: EdgeInsets.all(5),
-                        decoration: BoxDecoration(color: Colors.blue),
                       ),
-                    )
-                  ],
-                ),
-              );
-            }
-        ),
+                      padding: EdgeInsets.all(5),
+                      decoration: BoxDecoration(color: Colors.blue),
+                    ),
+                  ),
+
+
+                ],
+              ),
+            );
+          }),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.shopping_cart),
-        onPressed: (){
-          Navigator.push(context, MaterialPageRoute(builder: (context) => Cart(
+        onPressed: () {
+          if(counters.length > 0){
+            Navigator.push(
+                context, MaterialPageRoute(builder: (context) => Cart(
 
-          )));
+            )));
+
+          }
         },
-      )
-
-      ,
+      ),
     );
   }
 }
