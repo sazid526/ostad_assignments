e3132743f19cac2a7757c862630c36ea1dfabf62
sazid526 <arefin15-14526@diu.edu.bd>
2023-09-25 00:35:19 +0600

    done

--- lib/home.dart
+++ lib/home.dart
@@ -1,11 +1,18 @@
 import 'package:flutter/material.dart';
-import 'package:ostad_assignments/cart.dart';
 
-class Product {
-  final String name;
-  final double price;
+void main(){
+  runApp(MyApp());
+}
 
-  Product({required this.name, required this.price});
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Home(),
+    );
+  }
 }
 
 class Home extends StatefulWidget {
@@ -16,101 +23,92 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  List<Product> products = [
-    Product(name: "product 1", price: 10.0),
-    Product(name: "Product 2", price: 20.0),
-    Product(name: "product 3", price: 60.0),
-    Product(name: "Product 4", price: 20.0),
-    Product(name: "product 5", price: 90.0),
-    Product(name: "Product 6", price: 50.0),
-    Product(name: "product 7", price: 16.0),
-    Product(name: "Product 8", price: 80.0),
-    Product(name: "product 9", price: 60.0),
-    Product(name: "Product 10", price: 20.0),
-  ];
 
-  Map<String, int> counters ={};
+  int counter = 0;
 
-  MyAlartDialog(context) {
-    showDialog(
+  MyAlartDialog(context){
+    return showDialog(
         context: context,
-        builder: (BuildContext context) {
-          return AlertDialog(
-            title: Text("Successfull!"),
-            content: Text("You successfully buy product 5"),
-            actions: [
-              TextButton(
-                  onPressed: () {
-                    Navigator.pop(context);
-                  },
-                  child: Text("Ok"))
-            ],
+        builder: (BuildContext context){
+          return Expanded(
+              child: AlertDialog(
+                title: Text("Button pressed $counter times!"),
+                actions: [
+                  TextButton(onPressed: (){
+                    Navigator.of(context).pop();
+                  }, child: Text("Close"))
+                ],
+              )
           );
-        });
+        }
+    );
   }
-
-
   @override
   Widget build(BuildContext context) {
+
+
+
+
     return Scaffold(
-      appBar: AppBar(
-        title: Center(child: Text("Product List")),
-      ),
-      body: ListView.builder(
-          itemCount: products.length,
-          itemBuilder: (context, index) {
-            final product = products[index];
-            final counter = counters[product.name] ?? 0;
+        appBar: AppBar(
+          title: Text("Counter App"),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text("Count:",style: TextStyle(
+                  fontSize: 23
+              ),),
+              Text("$counter",style: TextStyle(
+                  fontSize: 45,
+                  fontWeight: FontWeight.bold
+              ),),
+              Center(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    ElevatedButton(
+                        onPressed: (){
+                          counter++;
+                          setState(() {
+                            if(counter == 5){
+                              MyAlartDialog(context);
+                            }
 
+                          });
 
-            return ListTile(
-              title: Text("${product.name}"),
-              subtitle: Text("\$${product.price}"),
-              trailing: Column(
-                children: [
-                  Text("Counter : $counter"),
-                  InkWell(
-                    onTap: () {
-                      if (counter == 5) {
-                        MyAlartDialog(context);
-                      }
-                      setState(() {
-                        if (counter < 5) {
-                          counters[product.name] = counter + 1;
-                        }
-                      });
-                    },
-                    child: Container(
-                      height: 30,
-                      width: 100,
-                      child: Center(
-                        child: Text(
-                          "Buy Now",
-                          style: TextStyle(color: Colors.white),
-                        ),
-                      ),
-                      padding: EdgeInsets.all(5),
-                      decoration: BoxDecoration(color: Colors.blue),
+                        },
+                        child: Icon(Icons.add)
                     ),
-                  ),
+                    SizedBox(width: 10,),
+                    ElevatedButton(
+                        onPressed: (){
+                          if(counter>0){
+                            counter --;
+                          }
 
 
-                ],
-              ),
-            );
-          }),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.shopping_cart),
-        onPressed: () {
-          if(counters.length > 0){
-            Navigator.push(
-                context, MaterialPageRoute(builder: (context) => Cart(
+                          setState(() {
+                            if(counter == 5){
+                              MyAlartDialog(context);
+                            }
 
-            )));
+                          });
 
-          }
-        },
-      ),
+                        },
+                        child: Icon(Icons.minimize_rounded)
+                    )
+                  ],
+                ),
+              )
+
+            ],
+          ),
+        )
     );
   }
 }
+
