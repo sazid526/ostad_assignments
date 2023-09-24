3385454b03ee7eeebbc911b8c1caaf65171744ef
sazid526 <arefin15-14526@diu.edu.bd>
2023-09-24 09:32:46 +0600

    done

--- lib/home.dart
+++ lib/home.dart
@@ -25,9 +25,12 @@ class _HomeState extends State<Home> {
             ],
           );
         });
+
   }
 
   int counter = 0;
+
+
   var product = [
     {"id": 1, "price": 25},
     {"id": 2, "price": 22},
@@ -40,54 +43,62 @@ class _HomeState extends State<Home> {
     {"id": 9, "price": 22},
     {"id": 10, "price": 90},
   ];
+  
+
+  List<int> counters = [];
 
   @override
   Widget build(BuildContext context) {
+
+
+
     return Scaffold(
         appBar: AppBar(
           title: Center(child: Text("Product List")),
         ),
         body: ListView.builder(
-            itemCount: product.length,
-            itemBuilder: (context, index) {
-              return Column(
-                children: [
-                  ListTile(
-                    title: Text("Product ${product[index]["id"]}"),
-                    subtitle: Text("\$${product[index]["price"]}"),
-                    trailing: Column(
-                      children: [
-                        Text("Counter : ${counter}"),
-                        InkWell(
-                          onTap: () {
-                            if (counter < 5) {
-                              counter = counter + 1;
-                            }
-                            if (counter == 5) {
-                              MyAlartDialog(context);
-                            }
+          itemCount: product.length,
+            itemBuilder: (context, index){
+              return ListTile(
+                title: Text("Product ${product[index]["id"]}"),
+                subtitle: Text("\$${product[index]["price"]}"),
+                trailing: Column(
+                  children: [
+                    Text("Counter : ${counter}"),
+                    InkWell(
+                      onTap: () {
+                        if(counter > 0){
+                          counters.add(counter);
+                        }
 
-                            setState(() {});
-                          },
-                          child: Container(
-                            height: 30,
-                            width: 100,
-                            child: Center(
-                              child: Text(
-                                "Buy Now",
-                                style: TextStyle(color: Colors.white),
-                              ),
-                            ),
-                            padding: EdgeInsets.all(5),
-                            decoration: BoxDecoration(color: Colors.blue),
+                        if (counter == 5) {
+                          MyAlartDialog(context);
+                        }
+
+                        setState(() {
+                          if (counter < 5) {
+                            counter = counter + 1;
+                          }
+                        });
+                      },
+                      child: Container(
+                        height: 30,
+                        width: 100,
+                        child: Center(
+                          child: Text(
+                            "Buy Now",
+                            style: TextStyle(color: Colors.white),
                           ),
-                        )
-                      ],
-                    ),
-                  ),
-                ],
+                        ),
+                        padding: EdgeInsets.all(5),
+                        decoration: BoxDecoration(color: Colors.blue),
+                      ),
+                    )
+                  ],
+                ),
               );
-            }),
+            }
+        ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.shopping_cart),
         onPressed: (){
