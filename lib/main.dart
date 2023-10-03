ffc9ebc3f85243f4979c33cb2f10cbdab718ad2f
sazid526 <arefin15-14526@diu.edu.bd>
2023-10-03 12:00:17 +0600

    done

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:ostad_assignments/home.dart';
+
 
 void main() {
   runApp(const MyApp());
@@ -11,7 +11,70 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Home(),
+      home: OrientationBuilder(
+          builder: (context, orientation){
+            if(orientation == Orientation.portrait){
+              return PortraitView();
+            }else{
+              return LandscapeView();
+            }
+          }
+      ),
     );
   }
 }
+
+class PortraitView extends StatelessWidget {
+  const PortraitView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("port"),
+      ),
+      body: ListView.builder(
+        itemCount: 10,
+        itemBuilder: (context, index) {
+          return Card(
+            child: Image.network(
+              'https://www.pexels.com/photo/double-exposure-of-a-woman-and-tagetes-flowers-13750614/',
+              height: 150,
+              width: 150,
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
+
+class LandscapeView extends StatelessWidget {
+  const LandscapeView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("land"),
+      ),
+      body: GridView.builder(
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+        ),
+        itemCount: 10,
+        itemBuilder: (context, index) {
+          return Card(
+            child: Image.network(
+              'https://www.pexels.com/photo/double-exposure-of-a-woman-and-tagetes-flowers-13750614/',
+              height: 150,
+              width: 150,
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
+
+
