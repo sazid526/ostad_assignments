ea4169c664d5cbd9dd42c0f1780931d63a639cea
sazid526 <arefin15-14526@diu.edu.bd>
2023-10-06 14:57:54 +0600

    done

---
+++ lib/homeScreen.dart
@@ -0,0 +1,188 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  List ItemList = [
+    {
+      "title": "Pullover",
+      "color": "Black",
+      "size": "L",
+      "price": 51,
+      "photo": "assets/images/photo.png",
+      "quantity": 1
+    },
+    {
+      "title": "T-Shirt",
+      "color": "Gray",
+      "size": "L",
+      "price": 30,
+      "photo": "assets/images/photoTwo.png",
+      "quantity": 1
+
+    },
+    {
+      "title": "Sport Dress",
+      "color": "Black",
+      "size": "M",
+      "price": 43,
+      "photo": "assets/images/photoThree.png",
+      "quantity": 1,
+    }
+  ];
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        actions: [
+          Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: Icon(Icons.search,color: Colors.black,),
+          )
+        ],
+        backgroundColor: Colors.white,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: Column(
+          children: [
+            SizedBox(
+              height: 40,
+            ),
+            Expanded(
+              flex: 10,
+              child: Text(
+                "My Bag",
+                style: TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            Expanded(
+              flex: 90,
+              child: ListView.builder(
+                  itemCount: ItemList.length,
+                  itemBuilder: (context, index) {
+                    return Container(
+                      height: 125,
+                      width: double.infinity,
+                      child: Card(
+                          child: Container(
+                        child: Row(
+                          children: [
+                            Image(image: AssetImage(ItemList[index]["photo"])),
+                            Expanded(
+                              child: Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Column(
+                                  children: [
+                                    Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.spaceBetween,
+                                      children: [
+                                        Text(
+                                          ItemList[index]["title"],
+                                          style: TextStyle(
+                                              fontSize: 18,
+                                              fontWeight: FontWeight.bold),
+                                        ),
+                                        Icon(Icons.more_vert)
+                                      ],
+                                    ),
+                                    Row(
+                                      children: [
+                                        Text(
+                                            "Color: ${ItemList[index]["color"]}"),
+                                        SizedBox(
+                                          width: 10,
+                                        ),
+                                        Text("Size: ${ItemList[index]["size"]}"),
+                                      ],
+                                    ),
+
+                                    Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.spaceBetween,
+                                      children: [
+                                        Row(
+                                          children: [
+                                            Card(
+                                              child: IconButton(
+                                                  onPressed: () {
+                                                    setState(() {
+                                                      if(ItemList[index]["quantity"] > 1){
+                                                        ItemList[index]["quantity"]--;
+                                                      }
+                                                    });
+                                                  },
+                                                  icon: Icon(Icons.remove)),
+                                              shape: CircleBorder(),
+                                            ),
+                                            Padding(
+                                              padding: const EdgeInsets.all(8.0),
+                                              child: Text(ItemList[index]["quantity"].toString(),style: TextStyle(
+                                                fontSize: 20,
+                                              ),),
+                                            ),
+                                            Card(
+                                              child: IconButton(
+                                                  onPressed: () {
+                                                    setState(() {
+                                                      ItemList[index]["quantity"]++;
+                                                    });
+                                                  },
+                                                  icon: Icon(Icons.add)),
+                                              shape: CircleBorder(),
+                                            )
+                                          ],
+                                        ),
+                                        Text("${ItemList[index]["price"] * ItemList[index]["quantity"]}\$")
+                                      ],
+                                    )
+                                  ],
+                                ),
+                              ),
+                            )
+                          ],
+                        ),
+                      )),
+                    );
+                  }),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text("Total amount: "),
+                Text("0\$")
+              ],
+            ),
+            SizedBox(height: 15,),
+            SizedBox(
+              width: double.infinity,
+              height: 40,
+              child: ElevatedButton(onPressed: (){
+
+              },
+                  child: Text("CHECK OUT"),
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.red,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(30))
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:ostad_assignments/homeScreen.dart';
 
 
 void main() {
@@ -11,135 +12,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: OrientationBuilder(
-          builder: (context, orientation){
-            if(orientation == Orientation.portrait){
-              return PortraitView();
-            }else{
-              return LandscapeView();
-            }
-          }
-      ),
+      home: HomeScreen(),
+      debugShowCheckedModeBanner: false,
+
     );
   }
 }
 
-class PortraitView extends StatelessWidget {
-  const PortraitView({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("port"),
-      ),
-      body: Column(
-        children: [
-          SizedBox(height: 5,),
-          CircleAvatar(
-            radius: 130,
-            backgroundImage: NetworkImage("https://images.pexels.com/photos/18427797/pexels-photo-18427797/free-photo-of-light-sea-dawn-landscape.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"),
-          ),
-          SizedBox(height: 10,),
-          Text("John Doe",style: TextStyle(
-              fontSize: 25,
-              fontWeight: FontWeight.bold
-          ),),
-          SizedBox(height: 10,),
-          Padding(
-            padding: const EdgeInsets.all(10.0),
-            child: Text("simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book Doe",style: TextStyle(
-              fontSize: 16,
-            ),),
-          ),
-          Expanded(
-            child: GridView.builder(
-              gridDelegate:  SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 3,
-                crossAxisSpacing: 2,
-              ),
-              itemCount: 9,
-              itemBuilder: (context, index) {
-                return Padding(
-                  padding: const EdgeInsets.all(10.0),
-                  child: Container(
-                    child: Image.network(
-                      'https://images.pexels.com/photos/18427797/pexels-photo-18427797/free-photo-of-light-sea-dawn-landscape.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',fit: BoxFit.cover,
-                      height: 50,
-                      width: 50,
-                    ),
-                  ),
-                );
-              },
-            ),
-          ),
-
-        ],
-      ),
-    );
-  }
-}
-
-class LandscapeView extends StatelessWidget {
-  const LandscapeView({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("land"),
-      ),
-      body: Row(
-        children: [
-          SizedBox(width: 5,),
-          CircleAvatar(
-            radius: 130,
-            backgroundImage: NetworkImage("https://images.pexels.com/photos/18427797/pexels-photo-18427797/free-photo-of-light-sea-dawn-landscape.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"),
-          ),
-          SizedBox(width: 10,),
-          Expanded(
-            child: Column(
-              children: [
-                Text("John Doe",style: TextStyle(
-                    fontSize: 25,
-                    fontWeight: FontWeight.bold
-                ),),
-                SizedBox(height: 10,),
-                Padding(
-                  padding: const EdgeInsets.all(10.0),
-                  child: Text("simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book Doe",style: TextStyle(
-                    fontSize: 16,
-                  ),),
-                ),
-                Expanded(
-                  child: GridView.builder(
-                    gridDelegate:  SliverGridDelegateWithFixedCrossAxisCount(
-                      crossAxisCount: 3,
-                      crossAxisSpacing: 2,
-                    ),
-                    itemCount: 9,
-                    itemBuilder: (context, index) {
-                      return Padding(
-                        padding: const EdgeInsets.all(10.0),
-                        child: Container(
-                          child: Image.network(
-                            'https://images.pexels.com/photos/18427797/pexels-photo-18427797/free-photo-of-light-sea-dawn-landscape.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',fit: BoxFit.cover,
-                            height: 50,
-                            width: 50,
-                          ),
-                        ),
-                      );
-                    },
-                  ),
-                ),
-              ],
-            ),
-          )
-
-        ],
-      ),
-    );
-  }
-}
 
 
